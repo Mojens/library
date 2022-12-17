b61b172b4a063f7368e123dd69b183c690a88306
mojens <moha135h@stud.kea.dk>
2022-12-17 18:55:55 +0100

    progress controller

---
+++ src/main/java/com/example/library/api/BookController.java
@@ -0,0 +1,30 @@
+package com.example.library.api;
+
+import com.example.library.service.BookService;
+import com.example.library.service.LoanService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+    private final LoanService loanService;
+
+    public BookController(BookService bookService, LoanService loanService) {
+        this.bookService = bookService;
+        this.loanService = loanService;
+    }
+
+
+
+
+
+
+
+
+
+}
